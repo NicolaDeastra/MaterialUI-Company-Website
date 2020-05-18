fdddfb3cd99cbd5ebbf87b8b1cb8e30db3daefa1
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-05-18 13:20:28 +0700

    set up nav router to and react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,46 @@
 import React from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import theme from "./ui/Theme";
 import Header from "./ui/Header";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      Hello
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route exact path="/services" component={() => <div>Services</div>} />
+          <Route
+            exact
+            path="/customsofware"
+            component={() => <div>Custom Software</div>}
+          />
+          <Route
+            exact
+            path="/mobileapps"
+            component={() => <div>Mobile Apps</div>}
+          />
+          <Route exact path="/websites" component={() => <div>Websites</div>} />
+          <Route
+            exact
+            path="/revolution"
+            component={() => <div>The revolution</div>}
+          />
+          <Route exact path="/about" component={() => <div>About us</div>} />
+          <Route
+            exact
+            path="/contact"
+            component={() => <div>Contact us</div>}
+          />
+          <Route
+            exact
+            path="/estimates"
+            component={() => <div>Estimates</div>}
+          />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
