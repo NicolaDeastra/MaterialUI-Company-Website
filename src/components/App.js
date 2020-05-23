207faa49a67a11ba47ea8671b72800f7026eabe7
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-05-23 14:21:13 +0700

    add menu to sevices tab

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ function App() {
           <Route exact path="/services" component={() => <div>Services</div>} />
           <Route
             exact
-            path="/customsofware"
+            path="/customsoftware"
             component={() => <div>Custom Software</div>}
           />
           <Route
