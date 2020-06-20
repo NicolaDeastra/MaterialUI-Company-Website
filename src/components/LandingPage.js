2facd6042ceb5cdd36de7e6a53f387083d30bb86
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-06-20 19:33:36 +0700

    finish service block styling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,6 +5,7 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import ButtonArrow from "./ui/ButtonArrow";
 import Typography from "@material-ui/core/Typography";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 import animationData from "../animations/landinganimation/data";
 import customSoftwareIcon from "../assets/Custom Software Icon.svg";
@@ -44,6 +45,9 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "0.7rem",
     height: 35,
     padding: 5,
+    [theme.breakpoints.down("sm")]: {
+      marginBottom: "2em",
+    },
   },
   mainContainer: {
     marginTop: "5em",
@@ -68,11 +72,24 @@ const useStyles = makeStyles((theme) => ({
   subtitle: {
     marginButton: "1em",
   },
+  icon: {
+    marginLeft: "2em",
+    [theme.breakpoints.down("xs")]: {
+      marginLeft: 0,
+    },
+  },
+  serviceContainer: {
+    marginTop: "12em",
+    [theme.breakpoints.down("sm")]: {
+      padding: 25,
+    },
+  },
 }));
 
 export default function LandingPage() {
   const classes = useStyles();
   const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
 
   const defaultOptions = {
     loop: true,
@@ -122,8 +139,19 @@ export default function LandingPage() {
       <Grid item>
         {" "}
         {/*-----Services Block-----*/}
-        <Grid container direction="row">
-          <Grid item>
+        <Grid
+          container
+          direction="row"
+          justify={matchesSM ? "center" : undefined}
+          className={classes.serviceContainer}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : "5em",
+              textAlign: matchesSM ? "center" : undefined,
+            }}
+          >
             <Typography variant="h4">Custom Software Development</Typography>
             <Typography variant="subtitle1" className={classes.subtitle}>
               Save Energy. Save Time. Save Money.
@@ -141,9 +169,13 @@ export default function LandingPage() {
               />
             </Button>
           </Grid>
-        </Grid>
-        <Grid item>
-          <img alt="custom software icon" src={customSoftwareIcon} />
+          <Grid item>
+            <img
+              className={classes.icon}
+              alt="custom software icon"
+              src={customSoftwareIcon}
+            />
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
